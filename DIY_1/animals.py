b805706f07e3562c576aaaefba35b6121c6eb315
Tife-code <soderubolu@gmail.com>
2021-03-18 17:24:32 +0100

    The week2 final DIYs

--- DIY_1/animals.py
+++ DIY_1/animals.py
@@ -3,4 +3,4 @@ Animals = ["Leopard", "Lion", "Black Panther"]
 for animal in Animals:
     print(f"{animal} is a cat, a big cat!")
 
-print("All these animals have whiskers, paws and they belong to the large cat family. ")
+print("All these animals have whiskers, paws and they belong to the big cat family. ")
